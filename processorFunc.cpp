494b0df87ba3052241f87d48f0da4b580a730dd8
Adrian McMenamin <acm538@york.ac.uk>
2016-11-22 12:19:26 +0000

    faster communication

--- processorFunc.cpp
+++ processorFunc.cpp
@@ -884,7 +884,7 @@ read_command:
     sub_(REG30, REG30, REG4);
 
     //wait longer if we have low processor number signalled
-    muli_(REG3, REG30, 0x75);
+    muli_(REG3, REG30, 0x20);
     tickReadingDown = proc->getProgramCounter();
 tick_read_down:
     proc->setProgramCounter(tickReadingDown);
@@ -965,7 +965,7 @@ wait_for_next_signal:
     cout << "Processor " << proc->getNumber() << " now waiting." << endl;
     push_(REG15);
     //try a back off
-    addi_(REG5, REG0, 0x400);
+    addi_(REG5, REG0, 0x40);
     addi_(REG6, REG0, 0x1000);
 
     waitingOnZero = proc->getProgramCounter();
@@ -1061,7 +1061,7 @@ wait_for_turn_to_complete:
     goto setup_loop_wait_processor_count;
 
 standard_delay:
-    addi_(REG4, REG0, 0x750);
+    addi_(REG4, REG0, 0x100);
 
 setup_loop_wait_processor_count:
     loopingWaitingForProcessorCount = proc->getProgramCounter();
