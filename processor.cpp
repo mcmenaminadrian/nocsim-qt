79d4211a95aee67801faf5b6214325df33fde601
Adrian McMenamin <acm538@york.ac.uk>
2017-03-09 22:30:23 +0000

    update random sequence code

--- processor.cpp
+++ processor.cpp
@@ -49,6 +49,7 @@ Processor::Processor(Tile *parent, MainWindow *mW, uint64_t numb):
     hardFaultCount = 0;
     smallFaultCount = 0;
     blocks = 0;
+        randomPage = 7;
 	inInterrupt = false;
     processorNumber = numb;
     clockDue = false;
@@ -341,7 +342,8 @@ const pair<const uint64_t, bool> Processor::getRandomFrame()
 	//See 3.2.1 of Knuth (third edition)
 	//simple ramdom number generator
 	//pick pages 3 - 14 (0 - 11)
-	randomPage = (3 * randomPage + 1)%12;
+        //old rand had factor of 3, better has factor of 1
+	randomPage = (1 * randomPage + 1)%12;
 	waitATick(); //store
 	return pair<const uint64_t, bool>(randomPage + 3, true);
 }
