7b1f09a00da84e458289e8ec06eb5eb539bd62d7
Adrian McMenamin <acm538@york.ac.uk>
2017-04-04 23:09:36 +0100

    balance MMU and tree again

--- mux.hpp
+++ mux.hpp
@@ -5,7 +5,7 @@
 
 //8 ticks plus MMU time 
 static const uint64_t MMU_DELAY = 4;
-static const uint64_t DDR_DELAY = 4;
+static const uint64_t DDR_DELAY = 8;
 static const uint64_t PACKET_LIMIT = 1;
 
 class Memory;
