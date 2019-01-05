b7cd7f5febcb513c1f834c0b9abef1c727ab2910
Adrian McMenamin <acm538@york.ac.uk>
2019-01-05 13:35:07 +0000

    flag adjustments

--- processor.cpp
+++ processor.cpp
@@ -607,10 +607,11 @@ uint64_t Processor::fetchAddressWrite(const uint64_t& address)
 					readLong(baseAddress + VOFFSET);
 				uint32_t oldFlags = masterTile->
 					readWord32(baseAddress + FLAGOFFSET);
-				if (!(oldFlags & 0x08)) {
-					waitATick();	
+				if (!(oldFlags & 0x05)) {
+					waitATick();
+					oldFlags = oldFlags ^ 0x08;	
 					masterTile->writeWord32(baseAddress +
-						FLAGOFFSET, oldFlags|0x08);
+						FLAGOFFSET, oldFlags|0x05);
 					waitATick();
 				}
 
